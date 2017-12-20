e6af392c45980d509019a9bcd9066b9844398ab3
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2017-12-20 14:47:55 +0300

    install main packages

---
+++ app/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => (
+  <div>
+    Hello  
+  </div>  
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+
+    context: path.resolve(__dirname, 'app'),
+
+    entry: './index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path:  path.resolve(__dirname, 'app'),
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    watch: false,
+
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react', 'stage-2']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css') 
+    ]
+};
