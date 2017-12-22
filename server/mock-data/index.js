07bd16ab3658aac5ee437d12961def6ad6dd4b09
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2017-12-22 16:16:08 +0300

    add jwt

---
+++ server/mock-data/index.js
@@ -0,0 +1,14 @@
+export default let data = [
+  {
+    id: '1',
+    username: 'User1',
+    email: 'User1@mail.ru',
+    password: 'User1pass',
+  },
+  {
+    id: '2',
+    username: 'User2',
+    email: 'User2@mail.ru',
+    password: 'User2pass',
+  },
+];
